9b80f7fab06a4a2894729ecdd8d8160af91d156a
Aryan Kumar <unique841426@gmail.com>
2026-03-13 20:24:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from huggingface_hub import InferenceClient
+from PIL import Image
+
+client=InferenceClient(token="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")
+Model="stabilityai/stable-diffusion-xl-base-1.0"
+
+st.title("AI Image Generator")
+st.write("Give your prompt to convert your imagination into image..")
+prompt=st.text_input("Describe your image")
+
+art_style = st.selectbox(
+    "Select an art style:",
+    ["Realistic", "Anime", "Cartoon"]
+)
+
+st.write("You selected:", art_style)
+
+
+
+if st.button("generate") and prompt:
+    with st.spinner("AI is generating your image..."):
+       image=client.text_to_image(prompt,model=Model)
+       st.image(image)
+
